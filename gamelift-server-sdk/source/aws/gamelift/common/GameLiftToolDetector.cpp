61ba83e00db489fd6c228e14f3f205cc20bc39d0
amayreh-gamelift <143558260+amayreh-gamelift@users.noreply.github.com>
2025-11-07 08:32:05 -0800

    Update GameLiftToolDetector.cpp to work with Unreal SDK

--- gamelift-server-sdk/source/aws/gamelift/common/GameLiftToolDetector.cpp
+++ gamelift-server-sdk/source/aws/gamelift/common/GameLiftToolDetector.cpp
@@ -22,19 +22,32 @@ void GameLiftToolDetector::SetGameLiftTool() {
     static constexpr const char* ENV_VAR_SDK_TOOL_NAME = "GAMELIFT_SDK_TOOL_NAME";
     static constexpr const char* ENV_VAR_SDK_TOOL_VERSION = "GAMELIFT_SDK_TOOL_VERSION";
     
-    const char* existingToolName = std::getenv(ENV_VAR_SDK_TOOL_NAME);
-    if ((existingToolName == nullptr || strlen(existingToolName) == 0) && IsToolRunning()) {
-        std::string toolName = GetToolName();
-        std::string toolVersion = GetToolVersion();
-        
-#ifdef _WIN32
-        _putenv_s(ENV_VAR_SDK_TOOL_NAME, toolName.c_str());
-        _putenv_s(ENV_VAR_SDK_TOOL_VERSION, toolVersion.c_str());
-#else
-        setenv(ENV_VAR_SDK_TOOL_NAME, toolName.c_str(), 1);
-        setenv(ENV_VAR_SDK_TOOL_VERSION, toolVersion.c_str(), 1);
-#endif
+    if (!IsToolRunning()) {
+        return;
     }
+    const char* existingToolName = std::getenv(ENV_VAR_SDK_TOOL_NAME);
+    const char* existingToolVersion = std::getenv(ENV_VAR_SDK_TOOL_VERSION);
+    std::string toolName = GetToolName();
+    std::string toolVersion = GetToolVersion();
+
+    if (existingToolName != nullptr && strlen(existingToolName) > 0) {
+        std::string existingToolNameStr(existingToolName);
+        std::string existingToolVersionStr(existingToolVersion != nullptr ? existingToolVersion : "");
+        if (existingToolNameStr.find(toolName) == std::string::npos) {
+            toolName = existingToolNameStr + "," + toolName;
+            toolVersion = existingToolVersionStr + "," + toolVersion;
+        } else {
+            toolName = existingToolNameStr;
+            toolVersion = existingToolVersionStr;
+        }
+    }
+#ifdef _WIN32
+    _putenv_s(ENV_VAR_SDK_TOOL_NAME, toolName.c_str());
+    _putenv_s(ENV_VAR_SDK_TOOL_VERSION, toolVersion.c_str());
+#else
+    setenv(ENV_VAR_SDK_TOOL_NAME, toolName.c_str(), 1);
+    setenv(ENV_VAR_SDK_TOOL_VERSION, toolVersion.c_str(), 1);
+#endif
 }
 
 } // namespace Common
